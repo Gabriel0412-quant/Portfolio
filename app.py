31a9d39e053f4dae0f1a322e88ad7943866559f7
Lugugmks <lg.diasdelima@gmail.com>
2025-05-12 17:24:11 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.title("Funções de texto")
+
+st.header(" Este é um header")
+
+st.subheader(" Este é um subheader")
+
+st.text(" Este é um texto")
+
+st.caption(" Este é um texto de legenda")
+
+st.markdown(" Este é um texto em markdown")
+
+st.markdown(" Este é um texto em markdown com **negrito** e _itálico_")
+st.markdown(" Este é um texto em markdown com ~~riscado~~")
+st.markdown(" Este é um texto em markdown com `código`")
+st.markdown(" Este é um texto em markdown com [link](https://www.google.com)")
+st.markdown('---')
+
+st.code('import streamlit as st', language='python')
+
+
+
